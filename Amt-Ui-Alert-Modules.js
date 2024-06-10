9559b9d97f17906a054f13ceeb7c9be1802040bd
amtcorp <141008517+amtcorp@users.noreply.github.com>
2024-06-10 18:00:34 +0200

    Add files via upload

---
+++ Amt-Ui-Alert-Modules.js
@@ -0,0 +1,98 @@
+ // This document is the property of AMT Corp. ALL RIGHTS RESERVED.
+
+let debugModeActivated = function() {
+if ( AmtUiversion === "pro" & debugwindow.classList.contains('active') ) {
+AmtUIAlertimageElement.src="./assets/icons/exclam2.png";
+alert ("import amt alertmodule (V1.0) : peut etre incompatible avec le système D'affichage d'Amt UI PRO");
+} else {
+
+}
+}
+
+// import Qrcode module
+const script2 = document.createElement('script');
+script2.src = 'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js';
+document.body.appendChild(script2);
+
+const displayboxstylemodule = document.createElement('link');
+displayboxstylemodule.rel = 'stylesheet';
+displayboxstylemodule.href = './Amt-Ui-Pro-Alert-Module.css';
+document.head.appendChild(displayboxstylemodule);
+
+const elementdisplaybox = document.createElement('div');
+elementdisplaybox.id = 'displaybox';
+
+const AmtUIAlertimageElement = document.createElement('img');
+AmtUIAlertimageElement.src = './assets/icons/exclam2.png';
+AmtUIAlertimageElement.style.maxHeight = '256px';
+
+const spanElement = document.createElement('span');
+spanElement.textContent = '';
+
+const buttonElement = document.createElement('button');
+buttonElement.id = 'closebutton';
+buttonElement.textContent = 'Close';
+
+
+elementdisplaybox.appendChild(AmtUIAlertimageElement);
+elementdisplaybox.appendChild(spanElement);
+elementdisplaybox.appendChild(buttonElement);
+document.body.appendChild(elementdisplaybox);
+
+function alert(msg) {
+    const displayBox = document.querySelector('#displaybox');
+AmtUIAlertimageElement.src='./assets/icons/exclam2.png';
+    displayBox.classList.remove('active-info'); // Supprimez la classe 'active-info' si elle est présente
+    displayBox.classList.add('active'); // Ajoutez la classe 'active'
+    document.querySelector('#displaybox span').innerHTML = msg;
+}
+
+// Ajoutez la fonction alert.info comme une propriété de alert
+alert.info = function(msg) {
+    const displayBox = document.querySelector('#displaybox');
+AmtUIAlertimageElement.src='./assets/icons/info.png'
+    displayBox.classList.remove('active'); // Supprimez la classe 'active' si elle est présente
+    displayBox.classList.add('active-info'); // Ajoutez la classe 'active-info'
+    document.querySelector('#displaybox span').innerHTML = msg;
+},
+
+alert.discover = function(msg) {
+    const displayBox = document.querySelector('#displaybox');
+AmtUIAlertimageElement.src='./assets/icons/info.png'
+    displayBox.classList.remove('active'); // Supprimez la classe 'active' si elle est présente
+    displayBox.classList.add('active-discover'); // Ajoutez la classe 'active-info'
+    document.querySelector('#displaybox span').innerHTML = msg;
+ setTimeout(() => {
+displayBox.classList.add('discover2');
+    }, 600);
+},
+
+alert.phonenumber = function(msg) {
+    const displayBox = document.querySelector('#displaybox');
+AmtUIAlertimageElement.src='./assets/icons/phone-call-white.png'
+    displayBox.classList.add('active'); // Supprimez la classe 'active' si elle est présente
+    displayBox.classList.remove('active-info'); // Ajoutez la classe 'active-info'
+    document.querySelector('#displaybox span').innerHTML = msg;
+    const QrElement = document.createElement('div');
+    QrElement.id = 'QRcode';
+    elementdisplaybox.appendChild(QrElement);
+
+  var qrcode = new QRCode(QRcode, {
+    text: "tel:" + msg, // replace with your URL
+    width: 128,
+    height: 128,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+  });
+
+
+};
+
+document.querySelector('#displaybox #closebutton').addEventListener('click', alertclose);
+
+function alertclose() {
+//document.querySelector('#displaybox').classList.remove('active');
+document.querySelector('#displaybox').className = '';
+document.querySelector('#displaybox span').innerHTML = '';
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+//import the amt alertmodule (V1.0)
+const script = document.createElement('script');
+script.src = './Amt-Ui-Alert-Modules.js';
+document.body.appendChild(script);
+
+window.onload = function () {
+}
