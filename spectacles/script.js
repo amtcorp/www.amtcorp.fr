8494cb5878ea84ba3042706057f598bdf2096420
amtcorp <141008517+amtcorp@users.noreply.github.com>
2024-09-08 20:36:09 +0200

    Add files via upload

--- spectacles/script.js
+++ spectacles/script.js
@@ -1,33 +1,82 @@
+ //© AMT corp & Keizer Sassin All rights reserved for more informations read readme.md
+// Utilisation non commerciale sans accord vente interdite
+
 const data = [
     {
         "imageName": "image 1",
-        "imageSrc": "./assets/images/Spectacles/1.jpg",
-        "title": "spectacle 1",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu ex scelerisque, fringilla quam ut, laoreet nisi. Fusce blandit metus sit amet semper elementum. Pellentesque in enim sed felis placerat molestie at ac quam. Praesent in erat quis mauris hendrerit sagittis. Morbi bibendum mauris ac ultricies pretium. Suspendisse posuere est eget eleifend maximus. Maecenas eleifend neque in blandit congue. Nunc porttitor ligula a leo malesuada elementum. Praesent mollis ipsum ac enim elementum, nec pellentesque quam pellentesque. <p></p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu ex scelerisque, fringilla quam ut, laoreet nisi. Fusce blandit metus sit amet semper elementum. Pellentesque in enim sed felis placerat molestie at ac quam. Praesent in erat quis mauris hendrerit sagittis. Morbi bibendum mauris ac ultricies pretium. Suspendisse posuere est eget eleifend maximus. Maecenas eleifend neque in blandit congue. Nunc porttitor ligula a leo malesuada elementum. Praesent mollis ipsum ac enim elementum, nec pellentesque quam pellentesque. ",
+        "imageSrc": "https://static.wixstatic.com/media/f0c249_9bda64197b00462f959a611ae3348971~mv2.jpg",
+        "title": "Polichinellerie - Affaire d'état",
+        "description": "Le monde du spectacle est averti que les représentations aux propos subversifs n&#39;ont plus caution et sont interdites. Le marionnettiste est prévenu , il doit changer de répertoire ou trouver un autre métier. Pour survivre notre héros légendaire , grand joueur et farceur devant l&#39; éternel doit faire face aux nouvelles lois et lubies étatiques. Polichinelle jouent à sa manière avec les personnages de notre temps qui veulent le bâillonner et le convertir.",
         "audience": "tout public",
-        "duration": "24min",
-        "date": "26/03/25",
-        "imagesSlider": "./assets/images/Spectacles/1.jpg, ./assets/images/Spectacles/2.jpg"
+        "duration": "40 mins",
+        "date": "en tournée",
+        "link": 'plaquette - Le secret de Polichinelle PDF.pdf',
+        "imagesSlider": "https://static.wixstatic.com/media/f0c249_9bda64197b00462f959a611ae3348971~mv2.jpg, https://static.wixstatic.com/media/f0c249_0330ab2e40844fa9a8b0a677d07eaa3f~mv2.jpg"
     },
     {
         "imageName": "image 2",
-        "imageSrc": "./assets/images/Spectacles/2.jpg",
-        "title": "spectacle 2",
-        "description": "text 2",
+        "imageSrc": "https://static.wixstatic.com/media/f0c249_d5bf1a6a247b4985a5e36f2b4d80d053~mv2.jpg",
+        "title": "Le secret de Polichinelle",
+        "description": "Dans cette nouvelle aventure le marionnettiste s&#39;interroge et veut changer son répertoire. Il propose à sa marionnette , Polichinelle de s&#39;adapter aux questions contemporaines. Polichinelle s&#39;en saisit et joue avec certains fait de société actuelles. S'ensuit une comédie burlesque de théâtre de marionnettes dans la tradition du théâtre de la foire, boniments, chansons et drôleries. Ce drôle de bonhomme au nez crochu, au dos bossu et aux mots railleurs, fait de vous les témoins et les complices de ses aventures époustouflantes. Polichinelle se jouent des personnages de notre temps: le pouvoir , la médecine et l&#39;ordre établi ... L&#39;action est au rendez-vous, rebondissements, &quot;bâtonnades&quot; et poursuites infernales rythment le jeu !",
         "audience": "tout public",
-        "duration": "40min",
-        "date": "27/03/25",
-        "imagesSlider": "./assets/images/Spectacles/3.jpg, ./assets/images/Spectacles/4.jpg"
+        "duration": "45 mins",
+        "date": "en tournée",
+        "link": 'plaquette - Le secret de Polichinelle PDF.pdf',
+        "imagesSlider": "https://static.wixstatic.com/media/f0c249_f905f2fa544f4d5e895d95ac53ac08d4~mv2.jpg, https://static.wixstatic.com/media/f0c249_d5bf1a6a247b4985a5e36f2b4d80d053~mv2.jpg, https://static.wixstatic.com/media/f0c249_9e0137529b764066bf54cc551d63c7d0~mv2.jpg"
     },
     {
         "imageName": "image 3",
-        "imageSrc": "./assets/images/Spectacles/3.jpg",
-        "title": "spectacle 3",
-        "description": "text 3",
+        "imageSrc": "https://static.wixstatic.com/media/f0c249_752458779b3e4f1094e1ed7f29d13fb2~mv2.jpg",
+        "title": "Drôles de Gaines",
+        "description": "Dans la tradition du théâtre forain, boniments, chansons et drôleries, voilà notre héros légendaire : Polichinelle ! Ce drôle de bonhomme au nez crochu, au dos bossu et aux mots railleurs, fait de vous les témoins et les complices de ses aventures rocambolesques, accompagné par sa bande de marionnettes traditionnelles : Pierrot, Arlequin, Colombine, Cassandre, le Gendarme, la mort, le crocodile... L&#39;action est au rendez-vous, rebondissements, &quot;bâtonnades&quot; et poursuites infernales rythment le jeu ! Les spectateurs sont sollicités par Polichinelle pour l&#39;aider à mener à bien ses projets fantasques ! Des clins d&#39;oeil au second degré amuseront les plus grands…",
         "audience": "tout public",
-        "duration": "55min",
-        "date": "27/03/25",
-        "imagesSlider": "./assets/images/Spectacles/5.jpg, ./assets/images/Spectacles/6.jpg"
+        "duration": "45 mins",
+        "date": "en tournée",
+        "link": "Droles de Gaines_bdf.pdf",
+        "imagesSlider": "https://static.wixstatic.com/media/f0c249_752458779b3e4f1094e1ed7f29d13fb2~mv2.jpg, https://static.wixstatic.com/media/f0c249_61a5fdd6723c434e857ab9aad94ab5b2~mv2.jpg, https://static.wixstatic.com/media/f0c249_1fab489bd49845e5afcf80b8e10af5f5~mv2.jpg"
+    },
+    {
+        "imageName": "image 4",
+        "imageSrc": "https://static.wixstatic.com/media/f0c249_d201cd0b318e46b5bc05ba92a3f8bf9e~mv2.jpg",
+        "title": "L'Atelier de Marionnettes",
+        "description": "Les participants sont accueillis par le marionnettiste qui leur présente le panorama de la marionnette et de ses techniques. Ils découvrent les différentes facettes et la richesse de la marionnette et de ses traditions dans le monde. Puis, nous abordons en situation, avec différents exercices, le travail sur la main et ses possibilités de mouvements pour servir la manipulation de la marionnette à gaine. Les marionnettistes en herbe découvrent sous leurs yeux émerveillés la grande famille des marionnettes de la compagnie Zouak avec les femmes et les hommes des cinq continents. Grâce à ces personnages, nous explorons les bases de la manipulation des marionnettes à gaine, leur jeu théâtral et leur mise en scène. Les participants peuvent en amont travailler sur un texte, un poème, une chanson ou une comptine… Ou alors des saynètes sont proposées ou créées et écrites. Et pour le plaisir de jouer, nous représentons le travail, les participants sont invités à faire part de leurs commentaires, critiques ou éloges.",
+        "duration": "2H",
+        "date": "sur demande",
+        "link": "Atelier_bdf.pdf",
+        "imagesSlider": "https://static.wixstatic.com/media/f0c249_d201cd0b318e46b5bc05ba92a3f8bf9e~mv2.jpg, https://static.wixstatic.com/media/f0c249_3088e315f276424fa8478d64d0b65916~mv2.jpg, https://static.wixstatic.com/media/f0c249_0fcd8e2ceef7434b81357330d8885b9d~mv2.jpg"
+    },
+    {
+        "imageName": "image 5",
+        "imageSrc": "https://static.wixstatic.com/media/f0c249_366f187b240b431a98962bb662a78de0~mv2.png",
+        "title": "Tribulations de Marionnettes au coeur des livres",
+        "description": "Un employé de médiathèque, Monsieur Jules Bouquin, fraichement engagé, arrive sur son nouveau lieu de travail, Il prend en charge au pied levé un groupe de spectateurs et lui fait visiter la médiathèque. Le travail de notre guide, agent de conservation improbable et passionné de littérature, consiste à ranger les livres mais à leur contact, il ne peut s’empêcher de basculer dans un jeu avec marionnettes et objets. S’ensuit une série de comédies et de jeux autour des livres qu’il présente aux visiteurs : une poésie de Boris Vian, un fait-divers du Midi Libre, quelques passages du Voyage de Gulliver de Jonathan Swift, des propos sur l’architecture go-thique et sur des gargouilles en pop-up, un extrait de Notre Dame de Paris de Victor Hugo, le monologue de l’Avare de Molière, et une interprétation contemporaine de Barbe Bleue d’après Charles Perrault. Plus de livres à ranger ? Chacun peut retourner à son quotidien…",
+        "audience": "tout public",
+        "duration": "45min",
+        "date": "sur demande",
+        "link": "Tribulations-2.pdf",
+        "imagesSlider": "https://static.wixstatic.com/media/f0c249_366f187b240b431a98962bb662a78de0~mv2.png, https://static.wixstatic.com/media/f0c249_b4777829da1044ef84755638513e7ec6~mv2.jpg, https://static.wixstatic.com/media/f0c249_97623626c48b4e9a94777bb273c955c1~mv2.png"
+    },
+    {
+        "imageName": "image 6",
+        "imageSrc": "https://static.wixstatic.com/media/f0c249_204fefad3efc4d1e8e19fda16eec4a1f~mv2.jpg",
+        "title": "Jaja ou la vie foutraque d'Alfred Jarry",
+        "description": "Le scandale provoqué par la pièce de Jarrry, Ubu Roi en 1896 continue encore aujourd’hui à faire des vagues. En utilisant de simples marionnettes, Jarry a réussi à dynamiter le théâtre de son temps. En le situant lui, l’auteur, face à ses écrits, ses parti-pris musicaux et son œuvre graphique, nous entendons nous inspirer de ses théories artistiques et montrer combien il reste notre contemporain. Jarry, mort à 34 ans, ne saura jamais qu’il exercera une influence importante sur la scène théâtrale et littéraire et sur bon nombre d’artistes qui se considèrent ses héritiers.<br/><br/>Synopsis : Alfred Jarry dans son modeste chez lui, nous dévoile ses marionnettes fétiches, le Père et la Mère Ubu. Le dialogue entre Jarry et ses personnages s’intensifie autour de cette question : écrire ou ne pas écrire ? Jarry nous expose en dansant sa vision esthétique du théâtre et de la marionnette, Il y a le double sens des mots et la logique absurde de la pataphysique… Alfred Jarry converse avec ses marionnettes, son hibou, ses bouteilles, son pistolet et son vélo… et tourne sa folle vie.",
+        "audience": "tout public",
+        "duration": "1H",
+        "date": "ne tourne plus",
+        "link": "web-jarry-distribution_bdf.pdf",
+        "imagesSlider": "https://static.wixstatic.com/media/f0c249_ccf15344179a434ba7fff669d8f5f529~mv2.jpg, https://static.wixstatic.com/media/f0c249_5a709bf377584f579ef9024df383108a~mv2.jpg, https://static.wixstatic.com/media/f0c249_d64d68ba5835400d916b32c8e493bfa4~mv2.jpg"
+    },
+    {
+        "imageName": "image 7",
+        "imageSrc": "./assets/images/Spectacles/3.jpg",
+        "title": "Le Petit Tailleur",
+        "description": "Ce célèbre conte issu de la culture et de l’imagination populaire, a été recueilli en Allemagne par les Frères grimm. Le compositeur Tibor Harsanyi l’a adapté et en a fait un conte musical. Le conte relate comment un simple petit tailleur plein d’astuces et de ruses épouse une princesse et devient roi au terme de toute une série d’aventures. Notre choix a été d’utiliser le texte du récitant pour le mettre en scène avec un comédien marionnettiste. Notre parti pris de jeu est de se laisser fasciné par les objets qui deviennent des personnages et pretextes à digressions poétiques et comiques...<br/><br/><i>Le comédien caracolant et piaffant à la manière des Monty Python est hilarant, inspiré et distrayant. Le public sourit béatement dans cette pseudo intimité, ou l’atelier finit dévasté par le combat des sangliers, les deux géants et la licorne..</i><br/><br/>Programme de l’Opéra junior de Montpellier",
+        "audience": "tout public",
+        "duration": "45min",
+        "date": "ne tourne plus",
+        "link": "Petit tailleur_bdf.pdf",
+        "imagesSlider": "https://static.wixstatic.com/media/f0c249_93ff0b511f074609b814bce958f2472f~mv2.jpg, https://static.wixstatic.com/media/f0c249_a1380829104c4874bb763d59bb76ae00~mv2.jpg, https://static.wixstatic.com/media/f0c249_04b359088ea64d0fa8cb885de7c0f553~mv2.jpg"
     }
 ];
 
@@ -44,6 +93,7 @@ const dateElement = document.getElementById('date');
 const spectacleSelect = document.getElementById('spectacle-select');
 const imageListElement = document.querySelector('.image-list');
 const pauseButtonImage = document.querySelector('.pausebutton img');
+const downloadButton = document.querySelector('#link');
 
 function updateSpectacle(index) {
     const item = data[index];
@@ -52,6 +102,12 @@ function updateSpectacle(index) {
     audienceElement.textContent = item.audience;
     durationElement.textContent = item.duration;
     dateElement.textContent = item.date;
+    downloadButton.href = "https://www.amtcorp.fr/compagnie%20zouak/plaquettes/" + item.link;
+    descriptionElement.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+
 
     currentImages = item.imagesSlider.split(', ');
     currentIndex = 0;
@@ -86,7 +142,7 @@ function prevImage() {
 }
 
 function startSlider() {
-    sliderInterval = setInterval(nextImage, 5000);
+    sliderInterval = setInterval(nextImage, 9000);
 }
 
 function stopSlider() {
@@ -99,10 +155,10 @@ function toggleSlider() {
     void pauseButtonImage.offsetWidth; // Trigger reflow to restart the animation
     pauseButtonImage.classList.add('bounce');
     if (sliderInterval) {
-        pauseButtonImage.src = "./assets/icons/play.png";
+        pauseButtonImage.src = "https://www.amtcorp.fr/spectacles/assets/icons/play.png";
         stopSlider();
     } else {
-        pauseButtonImage.src = "./assets/icons/pause.png";
+        pauseButtonImage.src = "https://www.amtcorp.fr/spectacles/assets/icons/pause.png";
         startSlider();
     }
 }
@@ -145,4 +201,13 @@ function generateImageList() {
 // Initial load
 populateSpectacleButtons();
 updateSpectacle(0);
-startSlider();
+
+if (/iPad|iPhone|iPod|Android/.test(navigator.userAgent)) {
+
+document.querySelector('.slider').style.display="none";
+document.querySelector('.image-list').style.display="none";
+
+} else {
+    startSlider();
+
+}
