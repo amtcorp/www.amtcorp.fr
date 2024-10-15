6649ebaac0fdde5abe7f683b69fa2fa42c17fd85
amtcorp <141008517+amtcorp@users.noreply.github.com>
2024-10-16 01:59:45 +0200

    Delete SmartMobile directory

--- SmartMobile/modules/Amt corp/debugmode.js
+++
@@ -1,9 +0,0 @@
-function debugmode() {
-    debugwindow = document.getElementById('debugwindow');
-    if ( debugwindow.classList.contains('active') ) {
-           document.querySelector('#debugwindow').classList.remove('active');
-    } else {
-           document.querySelector('#debugwindow').classList.add('active');
-           debugModeActivated();
-    }
-    }
